352d8e4473c1b900272920459c485070f359c609
AkharawitBuz <akharawitbuzzwoo@gmail.com>
2017-03-07 13:25:09 +0700

    Update index file
    I haves’t responsive.

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// Agency Theme JavaScript
+(function($) {
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); // End of use strict
+
+// scroll change logo src path
+$(document).ready(function() {
+    var scrollPos = 0;
+    $(window).scroll(function() {
+        var scrollPosCur = $(this).scrollTop();
+        if (scrollPosCur > 99) {
+            $('#navbrand').attr('src', 'images/logo.png');
+        } else {
+            $('#navbrand').attr('src', 'images/logo-bg.png');
+        }
+        // console.log(scrollPosCur);
+    });
+});
